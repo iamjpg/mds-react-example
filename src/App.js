1a18754b53bfe63a4dd8f0dfd6b3e97d9c1b5947
J.P. Given <jgiven@gmail.com>
2022-10-03 07:08:15 -0700

    Initial data retreived.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useState, useEffect } from 'react';
+import { MxButton, MxTable } from '@moxiworks/mds/react';
 import './App.css';
-import { MxButton } from '@moxiworks/mds/react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    const initialData = await fetch(
+      'https://anapioficeandfire.com/api/houses?page=1&pageSize=5'
+    );
+    const initialJson = await initialData.json();
+    setData(initialJson);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <main>
         <MxButton href='https://google.com' target='_blank'>
           Button as Link
         </MxButton>
+        <br />
+        <MxTable
+          serverPaginate
+          rowsPerPageOptions={[5, 10, 20, 50]}
+          rowsPerPage={5}
+          disablePagination={false}
+          rows={data}
+          columns={[
+            { property: 'name', heading: 'Name', sortable: false },
+            { property: 'region', heading: 'Region', sortable: false },
+            {
+              property: 'coatOfArms',
+              heading: 'Coat of Arms',
+              sortable: false,
+            },
+          ]}
+        />
       </main>
     </>
   );
